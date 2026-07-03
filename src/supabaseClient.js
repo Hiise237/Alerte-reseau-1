0a9c65a8a53ee1d3db5e690ff9e56f5233074471
Hiise237 <hiiseomar@gmail.com>
2026-07-03 02:20:35 +0100

    Delete src/supabaseClient.js

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://yjiyvtitwxdzfgjvohgz.supabase.co";
-const supabaseKey = "sb_publishable_VvRiHaAvYOe8w5ojiNcVhA_7Sn9Rth5";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
